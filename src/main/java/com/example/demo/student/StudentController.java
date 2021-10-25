5e952c8308996256ffabb47fddd056d305ca6679
nitinvishwari <nitinvishwari@gmail.com>
2021-10-25 02:35:15 -0400

    post apis

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +27,12 @@ public class StudentController {
     @RequestMapping(path = "/nitin")
     public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+
+    @PostMapping
+    @RequestMapping(path = "/add")
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
     }
 
 
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -21,4 +22,11 @@ public class StudentService {
         return studentRepository.findAll();
     }
 
+    public void addNewStudent(Student student) {
+        Optional<Student> optionalStudent = studentRepository.findStudentByEmail(student.getEmail());
+        if(optionalStudent.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+    }
 }
